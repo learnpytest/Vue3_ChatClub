1ffa6cb26742ddebc113e7f9c8a69ed81b11799d
RachelChen <iceage91@gmail.com>
2021-09-25 22:13:09 +0800

    Add signup feature Modified SigninSignup.vue component to add sign up with name, email and password feature based on firebase authentication

--- components/signin-signup-form/SigninSignup.vue
+++ components/signin-signup-form/SigninSignup.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="main">
     <div class="signup container">
-      <form action="" class="form signup__form">
+      <form action="" class="form signup__form" @submit.stop.prevent="signup">
         <h2>註冊 CHAT CLUB</h2>
         <div class="group">
           <label for="user_nickname" name="user_nickname" class="user_nickname"
@@ -11,6 +11,7 @@
             type="text"
             id="user_id"
             placeholder="Please enter your nickname"
+            v-model="signupUserNickName"
           />
         </div>
         <div class="group">
@@ -19,6 +20,7 @@
             type="email"
             id="email"
             placeholder="Please enter your email"
+            v-model="signupUserEmail"
           />
         </div>
         <div class="group">
@@ -30,6 +32,7 @@
             id="user_password"
             placeholder="Chat Club password should be at least 6 characters"
             autocomplete="off"
+            v-model="signupUserPassword"
           />
         </div>
         <div class="group">
@@ -44,6 +47,7 @@
             id="user_password--check"
             placeholder="Please enter your password again"
             autocomplete="off"
+            v-model="signupUserPasswordCheck"
           />
         </div>
         <div class="btn-group">
@@ -57,6 +61,73 @@
     </div>
   </div>
 </template>
+
+<script>
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import "@/config/db.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+// import { getDatabase } from "firebase/database";
+export default {
+  setup() {
+    const signupUserNickName = ref("");
+    const signupUserEmail = ref("");
+    const signupUserPassword = ref("");
+    const signupUserPasswordCheck = ref("");
+
+    const auth = getAuth();
+    // const db = getDatabase();
+    const router = useRouter();
+
+    const signup = async () => {
+      if (
+        !signupUserNickName.value ||
+        !signupUserEmail.value ||
+        !signupUserPassword.value ||
+        !signupUserPasswordCheck.value
+      ) {
+        return;
+      }
+      if (signupUserPassword.value !== signupUserPasswordCheck.value) {
+        // todo optimize alert ui
+        return alert("password not matched");
+      }
+      try {
+        const user = await createUserWithEmailAndPassword(
+          auth,
+          signupUserEmail.value,
+          signupUserPassword.value
+        );
+        const res = await updateProfile(auth.currentUser, {
+          displayName: signupUserNickName.value,
+          photoURL: `https://ui-avatars.com/api/?background=random&name=${signupUserNickName.value}`,
+        });
+        // todo optimize alert ui
+        alert("成功註冊");
+        router.push({
+          name: "sign-in",
+        });
+      } catch (err) {
+        // todo optimize alert ui
+        console.log("err", err);
+      }
+    };
+
+    return {
+      signupUserNickName,
+      signupUserEmail,
+      signup,
+      signupUserPassword,
+      signupUserPasswordCheck,
+    };
+  },
+};
+</script>
+
 <style lang="scss" scoped>
 @import url("https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i");
 
